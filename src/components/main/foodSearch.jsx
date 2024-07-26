97c43c01cbf2e83017dfab902bde21784e8c2248
dani0910 <dlekdms0910@naver.com>
2024-07-27 03:28:58 +0900

    food_serach

--- src/components/main/foodSearch.jsx
+++ src/components/main/foodSearch.jsx
@@ -8,10 +8,30 @@ const FoodSearch = () => { //css 개판으로 해놔서 수정해야함
   return (
     <>
         <Header/>
-        <main id="main">
+        <main class="main foodMain">
             <section id="foodSearchSection">
-                <button className="prevBtn">&lt; 아침</button>
-                
+                <h4 className="prevBtn">&lt; 아침</h4>
+                <div className="searchContainer">
+                  <input type="text" placeholder="음식 검색" />
+                  <button className="searchBtn"><span class="material-symbols-outlined">search</span></button>
+                </div>
+                <div className="resultContainer">
+                  <div className="tabs">
+                    <button className="allBtn">전체</button>
+                    <button className="bookmarkBtn">즐겨찾기</button>
+                    <button className="selfAddBtn">직접 추가</button>
+                  </div>
+                  <ul className="foodResultBox">
+                    <li>
+                      <input type="checkbox" />
+                      <div className="foodName">
+                        <p className="foodTxt">사과</p>
+                        <p className="gram">1개 (250개)</p>
+                      </div>
+                      <p className="foodKcal">142kcal</p>
+                    </li>
+                  </ul>
+                </div>
             </section>
         </main>
         <MenuBar/>
