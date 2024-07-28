076df6dd19825340aa8709796eb1a239ee744b45
dani0910 <dlekdms0910@naver.com>
2024-07-29 04:38:47 +0900

    app.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import IntroPage from "./components/Intro/intro";
 import SignUpPage1 from "./components/signup/signup1";
 import SignUpPage2 from "./components/signup/signup2";
 import SignUpPage3 from "./components/signup/signup3";
+import MainPage from "./components/main/main";
+import SelectFoodType from "./components/main/selectFoodType";
+import FoodSearch from "./components/main/foodSearch";
+import IngredientModal from "./components/main/ingredientModal";
 
 function App() {
   return (
@@ -14,10 +18,10 @@ function App() {
           <Route path="/signup1" element={<SignUpPage1 />}></Route>
           <Route path="/signup2" element={<SignUpPage2 />}></Route>
           <Route path="/signup3" element={<SignUpPage3 />}></Route>
-          <Route path="/main_page" element={<MainPage />}></Route>
-          <Route path="/food_type_select" element={<SelectFoodType />}></Route>
-          <Route path="/food_search" element={<FoodSearch />}></Route>
-          <Route path="/food_ingredient" element={<IngredientModal />}></Route>
+          <Route path="/home" element={<MainPage />}></Route>
+          <Route path="/home/food_type_select" element={<SelectFoodType />}></Route>
+          <Route path="/home/food_search" element={<FoodSearch />}></Route>
+          <Route path="/home/food_ingredient" element={<IngredientModal />}></Route>
         </Routes>
       </Router>
     </>
